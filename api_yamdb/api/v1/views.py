67c12371e2f838b1424032f99dd0250bdc297336
chaandrey <88089302+chaandrey@users.noreply.github.com>
2022-10-30 20:39:20 +0200

    Update views.py

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -11,7 +11,6 @@ from django.core.mail import send_mail
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from reviews.models import Category, Genre, Review, Title, User
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
@@ -23,6 +22,7 @@ from rest_framework.permissions import (AllowAny, IsAuthenticated,
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework_simplejwt.tokens import AccessToken
+from reviews.models import Category, Genre, Review, Title, User
 
 from api_yamdb.settings import EMAIL_FROM
 
