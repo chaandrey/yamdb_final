ec04c4d30c7bd6cde40bd8dd3eed8d76516d05d5
Andrey Charikov <andreycharikov@Andreys-MacBook-Pro-2.local>
2022-11-03 20:12:29 +0200

    42

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -68,16 +68,16 @@ WSGI_APPLICATION = "api_yamdb.wsgi.application"
 
 # Database
 
-DATABASES = { 
-    'default': { 
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'), 
-        'NAME': os.getenv('DB_NAME', 'postgres'), 
-        'USER': os.getenv('POSTGRES_USER', 'postgres'), 
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'), 
-        'HOST': os.getenv('DB_HOST', 'db'), 
-        'PORT': os.getenv('DB_PORT', '5432'), 
-    } 
-} 
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
+}
 
 
 # Password validation
